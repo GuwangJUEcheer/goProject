5c68d5afea16da70ed3602256c14b4ff38cbbc32
GuwangJUEcheer <1768595180@qq.com>
2020-07-16 21:29:28 +0800

    spring_boot

---
+++ src/main/java/cpm/edu/controller/DemoController.java
@@ -0,0 +1,16 @@
+package cpm.edu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/demo")
+public class DemoController {
+
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		return "Hello,spring boot";
+	}
+}
