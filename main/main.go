e7b0038ad37f8d2e8e68879c20423dffc7754b27
MYCOMPUTER\17685 <yiyi5205211314@gmail.com>
2024-02-25 12:55:02 +0900

    init函数

--- main/main.go
+++ main/main.go
@@ -2,9 +2,31 @@ package main
 
 //这个包一定要是main包
 //编译采用命令 go build -o bin xxx.exe(这里自定义) example.com/myproject/main(main所在路径前面gopath)
+//引入的最先
 import (
+	"fmt"
+
 	learnSlice "example.com/myproject/stringdemo"
+
+	"example.com/myproject/utils"
 )
+
+var meth = test()
+
+func test() int {
+
+	fmt.Println("全局变量定义")
+	return 0
+}
+
+// 全局变量定义最先 其次init
+func init() {
+
+	fmt.Println("这个函数在main前调用")
+	fmt.Println(meth)
+	//类似于string.format
+	fmt.Printf("age = %d 岁\n", utils.Age)
+}
 
 func main() {
 	learnSlice.ShowSlice()
---
+++ utils/utils.go
@@ -0,0 +1,12 @@
+package utils
+
+//演示创建init
+
+var Age int
+var Name string
+
+func init() {
+
+	Age = 1000
+	Name = "san"
+}
